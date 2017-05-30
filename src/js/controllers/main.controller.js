dcd3498c30117daa56d2b2cf560c008d36fc1c60
LBoyle <louis_boyle@hotmail.com>
2017-05-30 20:19:21 +0100

    changed how i was getting the current user in main controller

--- src/js/controllers/main.controller.js
+++ src/js/controllers/main.controller.js
@@ -2,24 +2,21 @@ angular
   .module('dinoApp')
   .controller('MainCtrl', MainCtrl);
 
-MainCtrl.$inject = ['$http', 'CurrentUserService', 'TokenService', 'User'];
-function MainCtrl($http, CurrentUserService, TokenService, User) {
+MainCtrl.$inject = ['$http', 'CurrentUserService', 'TokenService', '$rootScope', '$state'];
+function MainCtrl($http, CurrentUserService, TokenService, $rootScope, $state) {
   const vm = this;
 
-  // console.log(CurrentUserService.getUser());
-  if (TokenService.decodeToken()) {
-    User.get({id: TokenService.decodeToken().id})
-    .$promise.then(user => {
-      vm.currentUser = user;
-      console.log(vm.currentUser);
-    });
-  } else {
-    console.log('Not logged in');
-  }
-  // $http
-  //   .get('http://localhost:3000/api/races')
-  //   .then(response => {
-  //     vm.races = response.data;
-  //     console.log('vm.races:', vm.races);
-  //   });
+  $rootScope.$on('loggedIn', () => {
+    vm.user = CurrentUserService.currentUser;
+  });
+
+  $rootScope.$on('loggedOut', () => {
+    vm.user = null;
+    $state.go('login');
+  });
+
+  vm.logout = () => {
+    CurrentUserService.removeUser();
+  };
+
 }
